b70c6e1faef73c499607af18ed27dc753b983cc1
ComputerByte <46009342+ComputerByte@users.noreply.github.com>
2022-05-17 13:33:52 -0700

    fixes
    fixed nginx, disabled updater, general config cleanup

--- sonarr4kinstall.sh
+++ sonarr4kinstall.sh
@@ -14,7 +14,7 @@ user=$(_get_master_username)
 
 echo_progress_start "Making data directory and owning it to ${user}"
 mkdir -p "/home/$user/.config/sonarr4k"
-chown -R "$user":"$user" /home/$user/.config
+chown -R "$user":"$user" /home/$user/.config/sonarr4k
 echo_progress_done "Data Directory created and owned."
 
 echo_progress_start "Installing systemd service file"
@@ -46,18 +46,15 @@ echo_progress_done "Sonarr 4K service installed"
 if [[ -f /install/.nginx.lock ]]; then
     echo_progress_start "Installing nginx config"
     cat >/etc/nginx/apps/sonarr4k.conf <<-NGX
-    location /sonarr4k {
-        proxy_pass        http://127.0.0.1:8882/sonarr4k;
-        proxy_set_header Host \$proxy_host;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_redirect off;
-        auth_basic "What's the password?";
-        auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection \$http_connection;
-    }
+    location /sonarr {
+      proxy_pass        http://127.0.0.1:8882/sonarr4k;
+      proxy_set_header Host \$proxy_host;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto \$scheme;
+      proxy_redirect off;
+      auth_basic "What's the password?";
+      auth_basic_user_file /etc/htpasswd.d/htpasswd.${master};
+}
 NGX
     # Reload nginx
     systemctl reload nginx
@@ -65,25 +62,17 @@ NGX
 fi
 
 echo_progress_start "Generating configuration"
-# Start sonarr to config
-systemctl stop sonarr.service >>$log 2>&1
 
 cat > /home/${user}/.config/sonarr4k/config.xml << EOSC
 <Config>
-  <LogLevel>info</LogLevel>
-  <EnableSsl>False</EnableSsl>
   <Port>8882</Port>
   <SslPort>9898</SslPort>
   <UrlBase>/sonarr4k</UrlBase>
   <BindAddress>127.0.0.1</BindAddress>
-  <AuthenticationMethod>None</AuthenticationMethod>
-  <UpdateMechanism>BuiltIn</UpdateMechanism>
-  <Branch>main</Branch>
+  <UpdateMechanism>none</UpdateMechanism>
 </Config>
 EOSC
 chown -R ${user}:${user}/home/${user}/.config/sonarr4k/config.xml
-systemctl enable --now sonarr.service >>$log 2>&1
-sleep 20
 systemctl enable --now sonarr4k.service >>$log 2>&1
 
 echo_progress_start "Patching panel."
