6980a71db9c84d833547b22fdf2117f2a74946c7
poutine <11738103+drpoutine@users.noreply.github.com>
2022-05-17 22:02:48 -0500

    Fixes and Updated NGINX
    - Fixed Syntax Errors
    - Fixed Permissions Errors
    - Updated the NGINX conf to reflect new updates
    
    Disabled check_theD as its not dded and added systemd flags to ensure Panel's restart button actually works.
    
    Readded Dual sonarr instance stop and starts. weird things happen when its not there idk why ¯\_(ツ)_/¯

--- sonarr4kinstall.sh
+++ sonarr4kinstall.sh
@@ -46,14 +46,23 @@ echo_progress_done "Sonarr 4K service installed"
 if [[ -f /install/.nginx.lock ]]; then
     echo_progress_start "Installing nginx config"
     cat >/etc/nginx/apps/sonarr4k.conf <<-NGX
-    location /sonarr {
-      proxy_pass        http://127.0.0.1:8882/sonarr4k;
-      proxy_set_header Host \$proxy_host;
-      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-      proxy_set_header X-Forwarded-Proto \$scheme;
-      proxy_redirect off;
-      auth_basic "What's the password?";
-      auth_basic_user_file /etc/htpasswd.d/htpasswd.${master};
+location ^~ /sonarr4k {
+    proxy_pass http://127.0.0.1:8882;
+    proxy_set_header Host \$proxy_host;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Host \$host;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_redirect off;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection \$http_connection;
+    auth_basic "What's the password?";
+    auth_basic_user_file /etc/htpasswd.d/htpasswd.${user};
+}
+# Allow the API External Access via NGINX
+location ^~ /sonarr4k/api {
+    auth_basic off;
+    proxy_pass http://127.0.0.1:8882;
 }
 NGX
     # Reload nginx
@@ -63,16 +72,28 @@ fi
 
 echo_progress_start "Generating configuration"
 
+# Start sonarr to config
+systemctl stop sonarr.service >>$log 2>&1
+
 cat > /home/${user}/.config/sonarr4k/config.xml << EOSC
 <Config>
+  <LogLevel>debug</LogLevel>
+  <UpdateMechanism>BuiltIn</UpdateMechanism>
+  <Branch>develop</Branch>
+  <BindAddress>127.0.0.1</BindAddress>
   <Port>8882</Port>
   <SslPort>9898</SslPort>
-  <UrlBase>/sonarr4k</UrlBase>
-  <BindAddress>127.0.0.1</BindAddress>
-  <UpdateMechanism>none</UpdateMechanism>
+  <EnableSsl>False</EnableSsl>
+  <LaunchBrowser>False</LaunchBrowser>
+  <AuthenticationMethod>None</AuthenticationMethod>
+  <UrlBase>sonarr4k</UrlBase>
+  <UpdateAutomatically>False</UpdateAutomatically>
 </Config>
 EOSC
-chown -R ${user}:${user}/home/${user}/.config/sonarr4k/config.xml
+
+chown -R ${user}:${user} \/home/${user}/.config/sonarr4k/
+systemctl enable --now sonarr.service >>$log 2>&1
+sleep 10
 systemctl enable --now sonarr4k.service >>$log 2>&1
 
 echo_progress_start "Patching panel."
@@ -85,10 +106,11 @@ class sonarr4k_meta:
     pretty_name = "Sonarr 4K"
     baseurl = "/sonarr4k"
     systemd = "sonarr4k"
-    check_theD = True
+    check_theD = False
     img = "sonarr"
 class sonarr_meta(sonarr_meta):
-    check_theD = True
+    systemd = "sonarr"
+    check_theD = False
 EOF
 fi
 touch /install/.sonarr4k.lock >>$log 2>&1
